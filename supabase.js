4da5a5f4fdb49deb55b888247f5c615ef0064b57
thariq0219 <77294749+thariq0219@users.noreply.github.com>
2026-02-05 23:17:43 +0400

    git add

--- supabase.js
+++ supabase.js
@@ -1,4 +1,5 @@
-import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.94.0/+esm';
+
 
 export const supabase = createClient(
   'https://twzycmqwmqelwgnjiqmb.supabase.co',
